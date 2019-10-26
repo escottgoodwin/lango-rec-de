8970e5cbe001e163f4f2ed6dcbb718c374bf72fc
Evan Goodwin <evangoodwin@evans-mbp.home>
2019-10-26 09:39:00 -0700

    fixed error

--- generate_recs.py
+++ generate_recs.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup
 import traceback
 from requests.exceptions import HTTPError
 
-
 user = os.getenv('PGCONNECT_USER')
 password = os.getenv('PGCONNECT_PASSWORD')
 host = os.getenv('PGCONNECT_HOST')
